1437f3700dd0e18f1f438f97b9652ede3141aabe
ellenyuX <ellen.yu@computas.com>
2025-08-17 14:59:49 +0200

    added about

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,14 +1,23 @@
 import Navbar from "@/components/navbar/Navbar";
-import AboutPage from "@/pages/AboutPage/AboutPage";
 
 export default function About() {
   return (
     <div className="app-layout">
       <main className="main-content">
-        <AboutPage />
+          <h2>Ellen Yu hvem er det? idk, skal finne ut og komme tilbake til det</h2>
       </main>
     </div>
   );
 }
+
+import React from "react";
+
+const AboutPage: React.FC = () => {
+  return <h2>Ellen Yu hvem er det? idk, skal finne ut og komme tilbake til det</h2>;
+};
+
+export default AboutPage;
+
+
 
 
