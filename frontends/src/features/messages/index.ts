c0613c517de9bc5300b2f745b60745c233829bfe
ellenyuX <ellen.yu@computas.com>
2025-08-17 15:21:18 +0200

    remove shit

--- frontends/src/features/messages/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MessageForm } from "./components/MessageForm";
-export { default as MessageList } from "./components/MessageList";
-export { default as MessagePage } from "./pages/MessagePage";
