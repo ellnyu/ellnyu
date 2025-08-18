8344c5d1034066cadba016dd063907f6158957fc
ellenyuX <ellen.yu@computas.com>
2025-08-19 01:50:23 +0200

    navbar closing

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import styles from "./Navbar.module.scss";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+    const handleClose = () => setIsOpen(false);
 
   return (
     <nav className={styles.navbar}>
@@ -25,12 +26,12 @@ const Navbar = () => {
       </button>
 
       <ul className={`${styles.navLinks} ${isOpen ? styles.open : ""}`}>
-        <li><Link href="/posts">Posts</Link></li>
-        <li><Link href="/suggestions">Forslaaag</Link></li>
-        <li><Link href="/blog">Innlegg</Link></li>
-        <li><Link href="/books">Bøker</Link></li>
-        <li><Link href="/messages">Meldinger</Link></li>
-        <li><Link href="/about">Om meg</Link></li>
+        <li><Link href="/posts" onClick={handleClose}>Posts</Link></li>
+        <li><Link href="/suggestions" onClick={handleClose}>Forslaaag</Link></li>
+        <li><Link href="/blog" onClick={handleClose}>Innlegg</Link></li>
+        <li><Link href="/books" onClick={handleClose}>Bøker</Link></li>
+        <li><Link href="/messages" onClick={handleClose}>Meldinger</Link></li>
+        <li><Link href="/about" onClick={handleClose}>Om meg</Link></li>
       </ul>
     </nav>
   );
