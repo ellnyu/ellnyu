6859335447aa9d6617305b22e8572787b239ac66
ellenyuX <ellen.yu@computas.com>
2025-10-21 18:32:41 +0200

    better landing page cause i can't have halla twice heheheheeh

--- frontend/src/app/landing/page.tsx
+++ frontend/src/app/landing/page.tsx
@@ -4,9 +4,10 @@ import { Link } from "react-router-dom";
 const LandingPage: React.FC = () => {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Halla velkommen til mitt tragiske univers</h1>
+      <h1>Velkommen til mitt tragiske univers</h1>
       <p>
-        Jeg skal rydde på siden jeg lover
+        Bygget av meg, for meg og for andre interessenter som ønsker et innblikk i mitt tragiske liv, kode eller hva nå enn dette representerer.
+        Og jeg må rydde på siden jeg veeet men det er sååååå kjedeliiig hehehehhe
       </p>
     </div>
   );
